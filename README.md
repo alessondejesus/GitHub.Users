1a159cd170830077a38c56cb10016a21b1c9aaa0
alessondejesus <asn9006@hotmail.com>
2020-06-27 01:31:20 -0300

    Primeiro Modelo Funcional

---
+++ README.md
@@ -0,0 +1,26 @@
+####Requisição JS Usando Axios
+###### [Documentação do AXIOS](https://github.com/axios/axios "Documentação do AXIOS")
+------------
+
+> Axios é uma biblioteca JavaScript que permite **requisições** do lado do **cliente**. Podendo ser usado para Dev. Mobile, Single Page Aplication, API's internas e externas, servidor entre outros.
+
+###### Usando unpkg CDN:
+````html
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+````
+Importando a biblioteca com o script acima é possível usar os métodos e objetos do mesmo, como um simples:
+```javascript
+const resultado = axios.get('https://viacep.com.br/ws/45400000/json/')
+```
+É importante citar, também o uso de um **script** muito famoso e interessante que permite envio de formularios sem o famoso **refresh**, atráves de:
+```javascript
+var form = document.getElementById("formulario-usuario");
+form.addEventListener('submit', event => {
+    event.preventDefault()
+});
+```
+------------
+##### Bibliotecas e Frameworks Usados:
+- JQuery
+- BootStrap
+- Async/Await
---
+++ src/js/api.js
@@ -0,0 +1,71 @@
+/* Rodando funções ao clicar */
+const button = document.getElementById('botao')
+button.onclick = async () => {
+
+    /* Apagando valores padrões */
+    var ulModal = await document.getElementById('ulModal')
+    ulModal.innerHTML = ''
+    var titleModal = document.querySelector('h5#modal')
+    titleModal.innerHTML = ''
+
+    /* Capturando Input */
+    const user = await document.getElementById('usuario')
+    if (user.value.length <= 0){
+        user.style.borderColor = 'red'
+    }else{ 
+        executeRequest(user.value)    
+    }
+}
+
+
+async function executeRequest(url){
+    try{
+        /* url da requisição */
+        const response = await axios.get(`https://api.github.com/users/${url}`)
+        const data = {
+            'name': response.data.name,
+            'bio': response.data.bio,
+            'id': response.data.id
+        }
+        addModal(data)
+    }catch(err){
+        /* Erro */
+        console.log(err)
+        alert('Não pudemos atender à solicitação!')
+    }
+}
+
+function addModal(data){
+    /* Capturando campos */
+    var liModal = ''
+    var ulModal = ''
+
+    /* Incluindo Titulo */
+    const titleModal = document.querySelector('h5#modal')
+    titleModal.appendChild(document.createTextNode(document.getElementById('usuario').value))
+    ulModal = document.getElementById('ulModal')
+
+    /* Percorrendo Objecto */
+    for( let n in data){
+        if (data[n] == null){
+            data[n] = 'indisponível'
+        }
+        liModal = document.createElement('li')
+        liModal.setAttribute('class','list-group-item')
+        liModal.appendChild(document.createTextNode(`${data[n]}`))
+        ulModal.appendChild(liModal)
+    } 
+    /* Chamando Model */   
+    $('#modal').modal('show');
+}
+
+
+
+/* Não permite o refresh ao submitar */
+var form = document.getElementById("formulario-usuario");
+form.addEventListener('submit', event => {
+    event.preventDefault()
+});
+
+
+// 
